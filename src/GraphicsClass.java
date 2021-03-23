aaa6497a16775fcd1abb584fda2719bcd7075261
iamvs-2002 <iamvs2002@gmail.com>
2021-03-23 18:47:22 +0530

    Moving the snake

--- src/GraphicsClass.java
+++ src/GraphicsClass.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GraphicsClass extends JPanel {
+public class GraphicsClass extends JPanel implements KeyListener, ActionListener {
 
-    //length of snake along x and y axis
+    //position of snake along x and y axis
     private int[] snake_x_length = new int[500];
     private int[] snake_y_length = new int[500];
 
@@ -23,21 +27,26 @@ public class GraphicsClass extends JPanel {
     //number of moves initially =0
     private int moves = 0;
 
-    public GraphicsClass(){
-
+    public GraphicsClass()
+    {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
     public void paint(Graphics g){
 
         //border of the game
         g.setColor(Color.WHITE);
-        g.drawRect(12,12,760,640);
+        g.drawRect(24,24,651,631);
 
-        //the game frame, if the background has some other color
-//        g.setColor(Color.BLACK);
-//        g.fillRect(13,13,758,638);
+        //the game frame
+        g.setColor(Color.BLACK);
+        g.fillRect(25,25,650,630);
 
 
-        //initial postion of snake
+        //initial position of snake
         if (moves==0)
         {
             snake_x_length[0] = 100;
@@ -51,8 +60,6 @@ public class GraphicsClass extends JPanel {
             rightmouth = new ImageIcon("rightmouth.png");
             rightmouth.paintIcon(this,g,snake_x_length[0],snake_y_length[0]);
         }
-
-
 
         //drawing the snake
         for (int i=0;i<lengthOfSnake;i++){
@@ -84,6 +91,179 @@ public class GraphicsClass extends JPanel {
             }
         }
         g.dispose();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //from actionlistener interface
+        //starts when the timer begins
+
+        int i;
+        //checking direction
+        if (right){
+            System.out.println(moves);
+            for (i=lengthOfSnake-1; i>=0; i--){
+                //shifting the current position to next position
+                snake_y_length[i+1] = snake_y_length[i];
+            }
+
+            for (i=lengthOfSnake; i>=0; i--){
+                //if head, shift head by 25
+                if (i==0){
+                    snake_x_length[i]=snake_x_length[i]+25;
+                }
+                else{
+                    snake_x_length[i] = snake_x_length[i-1];
+                }
+                if (snake_x_length[i]>650){
+                    snake_x_length[i]=25;
+                }
+            }
+
+            repaint();
+        }
+        else if (left){
+            System.out.println(moves);
+            for (i=lengthOfSnake-1; i>=0; i--){
+                //shifting the current position to next position
+                snake_y_length[i+1] = snake_y_length[i];
+            }
+
+            for (i=lengthOfSnake; i>=0; i--){
+                //if head, shift head by 25
+                if (i==0){
+                    snake_x_length[i]=snake_x_length[i]-25;
+                }
+                else{
+                    snake_x_length[i] = snake_x_length[i-1];
+                }
+
+                if (snake_x_length[i]<25){
+                    snake_x_length[i]=650;
+                }
+            }
+
+            repaint();
+        }
+        else if (up){
+            System.out.println(moves);
+            for (i=lengthOfSnake-1; i>=0; i--){
+                //shifting the current position to next position
+                snake_x_length[i+1] = snake_x_length[i];
+            }
+
+            for (i=lengthOfSnake; i>=0; i--){
+                //if head, shift head by 25
+                if (i==0){
+                    snake_y_length[i]=snake_y_length[i]-25;
+                }
+                else{
+                    snake_y_length[i] = snake_y_length[i-1];
+                }
+
+                if (snake_y_length[i]<25){
+                    snake_y_length[i]=630;
+                }
+            }
+
+            repaint();
+        }
+        else if (down){
+            System.out.println(moves);
+            for (i=lengthOfSnake-1; i>=0; i--){
+                //shifting the current position to next position
+                snake_x_length[i+1] = snake_x_length[i];
+            }
+
+            for (i=lengthOfSnake; i>=0; i--){
+                //if head, shift head by 25
+                if (i==0){
+                    snake_y_length[i]=snake_y_length[i]+25;
+                }
+                else{
+                    snake_y_length[i] = snake_y_length[i-1];
+                }
+
+                if (snake_y_length[i]>630){
+                    snake_y_length[i]=25;
+                }
+            }
+
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //checking which key is pressed: up, down, left, right
+
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            //restart
+            moves=0;
+            lengthOfSnake=3;
+            repaint();
+        }
+
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_KP_RIGHT){
+            System.out.println(moves);
+            //right key is pressed
+            moves++;
+            if (!left)
+                right=true;
+            else{
+                left=true;
+                right=false;
+            }
+            up=false;
+            down=false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_KP_LEFT){
+            //right key is pressed
+            moves++;
+            if (!right)
+                left=true;
+            else{
+                left=false;
+                right=right;
+            }
+            up=false;
+            down=false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_KP_UP){
+            //right key is pressed
+            moves++;
+            if (!down)
+                up=true;
+            else{
+                down=true;
+                up=false;
+            }
+            left=false;
+            right=false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_KP_DOWN){
+            //right key is pressed
+            moves++;
+            if (!up)
+                down=true;
+            else{
+                up=true;
+                down=false;
+            }
+            left=false;
+            right=false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,17 @@ public class Main {
         //setting the frame
         JFrame jFrame = new JFrame();
         jFrame.setTitle("2D Snake Game");
-        jFrame.setBackground(Color.BLACK);
-        jFrame.setBounds(300,30,800,700);
-        jFrame.setResizable(false);
-        jFrame.setFont(Font.getFont(Font.SANS_SERIF));
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setVisible(true);
 
         //adding object of GraphicsClass as a component to the frame
         GraphicsClass graphicsClass = new GraphicsClass();
-        jFrame.add(graphicsClass);
+        jFrame.getContentPane().add(graphicsClass);
+
+        jFrame.setBackground(Color.BLACK);
+        jFrame.setBounds(300,30,715,745);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jFrame.setVisible(true);
 
     }
 }
